c35e412350f2e8689d5ec32b41a3775adc9906c5
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-05-02 20:27:55 +0530

    "navbar sorted"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import $ from "jquery";
 import logo from './logo.svg';
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css'
+import "materialize-css/dist/js/materialize.min.js";
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import Home from './Pages/Home'
+import Navbar from "./Components/Navbar"
+import Footer from "./Components/Footer"
+
+
+
+
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+          <Navbar />
+          <Switch>
+              <Route path="/" exact component={Home}    />
+            
+          </Switch>
+          <Footer />
+        </Router>
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import M from  'materialize-css/dist/js/materialize.min.js';
+
+
+
+
+
+
+
+
+
+export default function Navbar() {
+
+    
+    useEffect(()=>{
+    let sidenav = document.querySelector('.sidenav');
+      M.Sidenav.init(sidenav, {
+        isFixed:true  
+      });
+    },[])
+
+   
+
+    return (
+        <div class="">
+            <nav class="nav-extended blue">
+            <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Logo</a>
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+            </div>
+            
+        </nav>
+
+        <ul class="sidenav" id="mobile-demo">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+        </ul>
+        </div>
+    )
+}
