a85bdfb22b111ca00b2af1e8b075a4c88e801f6d
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-05-02 23:14:22 +0530

    "Home page"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React,{useEffect} from 'react';
 import $ from "jquery";
 import logo from './logo.svg';
-import './App.css';
 import 'materialize-css/dist/css/materialize.min.css'
 import "materialize-css/dist/js/materialize.min.js";
+import './App.css';
+
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 import Home from './Pages/Home'
 import Navbar from "./Components/Navbar"
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Heroimage from '../images/heroimage.png'
 
 export default function Home() {
     return (
@@ -6,7 +7,25 @@ export default function Home() {
 
 
         <div >
-            Home component
+            <section className="homecontainer section" id="home"> 
+                <div className="row ">
+                    <div className="col l6 m12 s12 section landingpagediv">
+                        <h1 className="left-align valign-wrapper">WELCOME TO THE WORLD OF EDUNOMICS</h1>
+                        <div  className="d-flex">
+                        <input type="text" placeholder="Enter your search" className="search"></input>
+
+                        </div>
+                        <div className="d-flex">
+                            <button className="button pink waves-effect lighten-1 white-text">Learning </button>
+                            <button className="button waves-effect white-text light-green accent-4 right">Career </button>
+                        </div>
+                    </div>
+                    <div className=" col l6 m12 s12 ">
+                        <img src={Heroimage}  className="img-responsive heroimage"></img>zxc
+                    </div>
+
+                </div>
+            </section>
         </div>
     )
 }
