69f3ba8ccf134bf21ba216f9dd11b2b96d3b7505
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-05-03 20:19:08 +0530

    "second level form done"

--- src/Form/SecondForm.js
+++ src/Form/SecondForm.js
@@ -1,15 +1,66 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import M from  'materialize-css/dist/js/materialize.min.js';
 
-export default function SecondForm() {
+
+
+export default function SecondForm(props) {
+    
+    useEffect(()=>{
+        let select = document.querySelector('select');
+          M.FormSelect.init(select, {});
+        },[])
+    
+    const conti = e => {
+            e.preventDefault();
+            props.nextStep();
+    };
+    
+    
+    
     return (
         <div  className="container lower">
            <form>
                <div className="row">
-                   <div className="col s12">
-                        <h6>What aspirations you have with this Internship ?</h6>
+                   <div className="col input-field s12">
                         <textarea id="textarea1" className="materialize-textarea" />
-                        <label for="textarea1">Textarea</label>
+                        <label for="textarea1">What aspirations you have with this Internship ?</label>
                    </div>
+                   
+                   <div className="col input-field s12">
+                        <textarea id="textarea1" className="materialize-textarea" />
+                        <label for="textarea1">What aspirations you have with this Internship ?</label>
+                   </div>                   
+                   
+                   <div className="col input-field s12">
+                        <textarea id="textarea1" className="materialize-textarea" />
+                        <label for="textarea1">What aspirations you have with this Internship ?</label>
+                   </div>                   
+                   
+                   <div class="input-field col s12">
+                    
+                    <select multiple>
+                    <option value="" disabled selected>Choose your option</option>
+                    <option value="1">Salary & Compensation</option>
+                        <option value="2">Challenging environment of startup
+</option>
+                    <option value="3">Learning</option>
+                    <option value="3">Work for vision to transform Indian Education & Economy</option>
+
+                    </select>
+                    <label>Are you motivated with our vision or </label>
+                </div>
+                   
+                   
+                   
+                   <div className="col input-field s12">
+                        <textarea id="textarea1" className="materialize-textarea" />
+                        <label for="textarea1">What aspirations you have with this Internship ?</label>
+                   </div>
+                   <div className="col s12 center">
+                    <button type="submit" className="btn large light-green accent-4" onClick={conti}>Next<i class="material-icons right">arrow_forward</i></button>
+                </div>
+
+
 
                </div>
            </form>
---
+++ src/Form/ThirdForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ThirdForm() {
+    return (
+        <div>
+            third form
+        </div>
+    )
+}
