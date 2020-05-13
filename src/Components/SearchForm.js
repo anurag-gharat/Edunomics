1fa5b507f27920547c03a2e3177928a540a62c9c
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-05-14 01:48:22 +0530

    set search working

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -8,7 +8,12 @@ export default function SearchForm() {
 
     const [query,setQuery] = useState('')
     const [redirect, setRedirect] = useState(false)
-  
+    const [search,setSearch]=useState("career")
+    
+    const activeSearch="btn light-green accent-4 activebtn"
+    const inactiveSearch="btn light-green accent-4 " 
+
+
     const handleSubmit=(e)=>{
         e.preventDefault();
         if (query===''){
@@ -35,7 +40,10 @@ export default function SearchForm() {
         },[])
     
 
-
+    const handleRole=(e)=>{
+        e.preventDefault()
+        setSearch(e.target.name)
+    }
  
  
     const handleChange=(e)=>{
@@ -51,15 +59,23 @@ export default function SearchForm() {
         <form onSubmit={(e)=>handleSubmit(e)}>
             
         <div className="row m-t-10">
-            <div className="col s12 l7">
+            <div className="col s12 l10">
                 
                     <div className="input-field">
                         <input type="text" id="autocomplete-input" className="autocomplete" onChange={handleChange} />
                         <label htmlFor="autocomplete-input">Enter Your Query</label>
                 </div>
             </div>
-            <div className="col s12 l5 m-t-10">
-                <button type="submit" className="button m-t-10 gradient1 white-text hoverable">Search</button>
+            <div className="col s12 l2 m-t-10">
+                <button type="submit" className="button-search m-t-10 gradient1 white-text hoverable">Search</button>
+            </div>
+            <div className="row">
+                <div className="col l6 s6 right-align">
+                    <button className={search==="career"?activeSearch:inactiveSearch} name="career" onClick={handleRole}>Career</button>
+                </div>
+                <div className="col l6 s6 left-align">
+                    <button className={search==="skill"?activeSearch:inactiveSearch} name="skill" onClick={handleRole}  >Skill</button>
+                </div>
             </div>
         </div>
         </form>
