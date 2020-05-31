b01c1a62f6374a9227832077cb22947f917d3fe8
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-05-31 15:22:42 +0530

    resume builder set up

--- src/Pages/UserResume.js
+++ src/Pages/UserResume.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Home from '../Resume/Components/Home'
 export default function UserResume() {
     return (
-        <div className="resume-screen p-t-100">
+        <div className="resume-screen p-t-100 m-b-50">
             <Home />
         </div>
     )
--- src/Resume/Layout/index.js
+++ src/Resume/Layout/index.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Navbar from "./Navbar";
 
 export default function PageLayout({ children }) {
   return (
     <React.Fragment>
-      <Navbar />
       {children}
     </React.Fragment>
   );
